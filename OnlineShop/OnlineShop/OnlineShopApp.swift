a9cbb4203a16d281e7f15393709791ec82ce23ad
gorka <gorka.uriarte@ikasle.egibide.org>
2024-02-05 20:54:06 +0100

    Proyecto inicial

---
+++ OnlineShop/OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by  on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
