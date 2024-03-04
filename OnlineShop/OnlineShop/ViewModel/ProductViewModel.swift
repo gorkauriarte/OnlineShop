38bf1a3717041597d97ea94fa0c2ce407941a886
gorka <gorka.uriarte@ikasle.egibide.org>
2024-03-04 18:55:17 +0100

    Lun 4 inicio

--- OnlineShop/OnlineShop/ViewModel/ProductViewModel.swift
+++ OnlineShop/OnlineShop/ViewModel/ProductViewModel.swift
@@ -7,32 +7,42 @@
 
 import Foundation
 
-func getProduct(url:String){
-        Task{ //hace que sea asíncrona la tarea, consiguiendo concurrencia
-            do{
-                let product = try await NetworkManager.shared.getProduct(url: url)
-                // self.products = product
-            }catch{
-                
-                if let callError = error as? WEError {
-                    switch callError{
-                    case .invalidURL:
-                        print("Invalid URL")
-                    case .invalidResponse:
-                        print("Invalid response")
-                    case .invalidData:
-                        print("Invalid data")
-                    case .unableToComplete:
-                        print("Unable to complete")
-                    }
+@MainActor
+class ProductViewModel: ObservableObject{
+    
+    @Published var tiempo: [Product] = []
+    
+    init(){
+        getProduct(url: "&q=Gasteiz&days=5"); // CAMBIAR
+    }
+    
+    func getProduct(url:String){
+            Task{ //hace que sea asíncrona la tarea, consiguiendo concurrencia
+                do{
+                    let weather = try await NetworkManager.shared.getProduct(url: url)
+                    self.tiempo.append(weather)
+                }catch{
                     
-                }else{
-                    //Generic error
-                    print("Invalid response")
+                    if let callError = error as? WEError {
+                        switch callError{
+                        case .invalidURL:
+                            print("Invalid URL")
+                        case .invalidResponse:
+                            print("Invalid response")
+                        case .invalidData:
+                            print("Invalid data")
+                        case .unableToComplete:
+                            print("Unable to complete")
+                        }
+                        
+                    }else{
+                        //Generic error
+                        print("Invalid response")
+                    }
                 }
             }
         }
-    }
+}
 
 
 
